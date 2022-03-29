92791b30289562e6163ba795ab979374e9f46736
egekibar <58031957+egekibar@users.noreply.github.com>
2022-03-29 16:12:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
-<?php 
- 
- function tarihtenYuzde($startTime, $endTime){
- 
-   $startDays = (strtotime($startTime)  / (60 * 60 * 24));
-   $nowDays = (time()  / (60 * 60 * 24));
-   $endDays = (strtotime($endTime)  / (60 * 60 * 24)));
-   
-   ceil((time()  / (60 * 60 * 24)) - (strtotime($a)  / (60 * 60 * 24)))
-   ceil((strtotime($b)  / (60 * 60 * 24)) - (strtotime($a)  / (60 * 60 * 24)))
-   
-   yuzde($a,$b){
-    $c = $b / 100;
-    return floor($a / ($c));
-   
- }
+<?php
+
+  // Fonksyon
+  function tarihtenYuzde($startTime, $endTime){
+
+    $startDays = (strtotime($startTime)  / (60 * 60 * 24));
+    $nowDays = (time()  / (60 * 60 * 24));
+    $endDays = (strtotime($endTime)  / (60 * 60 * 24)));
+    return ceil(ceil($nowDays - $startDays) / (ceil($endDays - $startDays) / 100));
+
+  }
+
+  // Kullanımı
+  tarihtenYuzde(,)
